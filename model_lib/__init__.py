4c988ec4a6c033f9de5b25cbf42424ccbb1f775d
Gunel Aliyeva <90043330+gunelaliyevaa@users.noreply.github.com>
2025-02-13 00:35:58 +0400

    Update __init__.py

--- model_lib/__init__.py
+++ model_lib/__init__.py
@@ -1,5 +1,5 @@
 from .data_loader import count_images_in_directory, create_data_generators
-from .model_builder import build_model
+from .builder import build_model
 from .training import train_model, fine_tune_model
 from .evaluation import evaluate_model, get_predictions, evaluate_predictions
 from .preprocessing import preprocess_image
