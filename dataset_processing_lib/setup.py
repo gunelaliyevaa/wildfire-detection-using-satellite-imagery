8d004b20e5ea28f3fb1530e75098f59c59c55453
turgay farhadli <113557386+phturgay1@users.noreply.github.com>
2025-02-13 00:18:33 +0400

    Create setup.py

---
+++ dataset_processing_lib/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dataset_processing_lib',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List any external dependencies here
+    ],
+)
