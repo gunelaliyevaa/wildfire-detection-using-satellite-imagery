62231a2bd150966b8bb7078a470da3f63978cd61
turgay farhadli <113557386+phturgay1@users.noreply.github.com>
2025-02-14 02:24:30 +0400

    Update __init__.py

--- model_lib/__init__.py
+++ model_lib/__init__.py
@@ -2,4 +2,5 @@ from .data_loader import count_images_in_directory, create_data_generators
 from .builder import build_model
 from .training import train_model, fine_tune_model
 from .preprocessing import preprocess_image
+from .predictor import predict_image
 from .utils import plot_training_history, plot_fine_tuning_history, save_model
